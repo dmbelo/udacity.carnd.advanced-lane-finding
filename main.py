9b55b4d884c33d07df718ea28359f3f702f78d4f
dmbelo <david.belo@mclaren.com>
2017-02-14 13:51:27 -0800

    Lane searching started

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import cv2
 import json
 import glob
 import matplotlib.pyplot as plt
+import ipdb
 
 
 class Camera():
@@ -50,12 +51,12 @@ class Camera():
         return cv2.undistort(img, self.mtx, self.dtx, None, self.mtx)
 
     def plan_view(self, img):
-        flag = cv2.INTER_LINEAR
-        return cv2.warpPerspective(img, self.M_plan, self.img_size, flags=flag)
+        f = cv2.INTER_LINEAR
+        return cv2.warpPerspective(img, self.M_plan, self.img_size, flags=f)
 
     def front_view(self, img):
-        flag = cv2.INTER_LINEAR
-        return cv2.warpPerspective(img, self.M_front, self.img_size, flags=flag)
+        f = cv2.INTER_LINEAR
+        return cv2.warpPerspective(img, self.M_front, self.img_size, flags=f)
 
 
 class Line():
@@ -83,16 +84,170 @@ class Line():
 
 
 class Lane():
-    def __init__(self, left, right, width):
-        self.left_line = left
-        self.right_line = right
+    def __init__(self, width):
+        self.left_line = Line()
+        self.right_line = Line()
         self.width = width
 
     def blind_search(self, binary):
-        raise NotImplementedError
+        # Take a histogram of the bottom half of the image
+        histogram = np.sum(binary[binary.shape[0]//2:, :], axis=0)
+
+        # Create an output image to draw on and  visualize the result
+        out_img = np.dstack((binary, binary, binary))*255
+        # Find the peak of the left and right halves of the histogram
+        # These will be the starting point for the left and right lines
+        midpoint = np.int(histogram.shape[0]/2)
+        leftx_base = np.argmax(histogram[:midpoint])
+        rightx_base = np.argmax(histogram[midpoint:]) + midpoint
+
+        # Choose the number of sliding windows
+        nwindows = 9
+        # Set height of windows
+        window_height = np.int(binary.shape[0]/nwindows)
+        # Identify the x and y positions of all nonzero pixels in the image
+        nonzero = binary.nonzero()
+        nonzeroy = np.array(nonzero[0])
+        nonzerox = np.array(nonzero[1])
+        # Current positions to be updated for each window
+        leftx_current = leftx_base
+        rightx_current = rightx_base
+        # Set the width of the windows +/- margin
+        margin = 100
+        # Set minimum number of pixels found to recenter window
+        minpix = 50
+        # Create empty lists to receive left and right lane pixel indices
+        left_lane_inds = []
+        right_lane_inds = []
+
+        # Step through the windows one by one
+        for window in range(nwindows):
+            # Identify window boundaries in x and y (and right and left)
+            win_y_low = binary.shape[0] - (window+1)*window_height
+            win_y_high = binary.shape[0] - window*window_height
+            win_xleft_low = leftx_current - margin
+            win_xleft_high = leftx_current + margin
+            win_xright_low = rightx_current - margin
+            win_xright_high = rightx_current + margin
+            # Draw the windows on the visualization image
+            cv2.rectangle(out_img, (win_xleft_low, win_y_low),
+                          (win_xleft_high, win_y_high), (0, 255, 0), 2)
+            cv2.rectangle(out_img, (win_xright_low, win_y_low),
+                          (win_xright_high, win_y_high), (0, 255, 0), 2)
+            # Identify the nonzero pixels in x and y within the window
+            good_left_inds = ((nonzeroy >= win_y_low) &
+                              (nonzeroy < win_y_high) &
+                              (nonzerox >= win_xleft_low) &
+                              (nonzerox < win_xleft_high)).nonzero()[0]
+            good_right_inds = ((nonzeroy >= win_y_low) &
+                               (nonzeroy < win_y_high) &
+                               (nonzerox >= win_xright_low) &
+                               (nonzerox < win_xright_high)).nonzero()[0]
+            # Append these indices to the lists
+            left_lane_inds.append(good_left_inds)
+            right_lane_inds.append(good_right_inds)
+            # If you found > minpix pixels, recenter next window on mean pos.
+            if len(good_left_inds) > minpix:
+                leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
+            if len(good_right_inds) > minpix:
+                rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+
+        # Concatenate the arrays of indices
+        left_lane_inds = np.concatenate(left_lane_inds)
+        right_lane_inds = np.concatenate(right_lane_inds)
+
+        # Extract left and right line pixel positions
+        leftx = nonzerox[left_lane_inds]
+        lefty = nonzeroy[left_lane_inds]
+        rightx = nonzerox[right_lane_inds]
+        righty = nonzeroy[right_lane_inds]
+
+        # Fit a second order polynomial to each
+        left_fit = np.polyfit(lefty, leftx, 2)
+        right_fit = np.polyfit(righty, rightx, 2)
+        # If there are more points on one lane it should be weighted accordingly
+
+        # PLOTTING
+        # Generate x and y values for plotting
+        ploty = np.linspace(0, binary.shape[0]-1, binary.shape[0])
+        left_fitx = left_fit[0] * ploty ** 2 + left_fit[1] * ploty + left_fit[2]
+        right_fitx = right_fit[0] * ploty ** 2 + right_fit[1] * ploty + right_fit[2]
+
+        out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
+        out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
+        plt.imshow(out_img)
+        plt.plot(left_fitx, ploty, color='yellow')
+        plt.plot(right_fitx, ploty, color='yellow')
+        plt.xlim(0, 1280)
+        plt.ylim(720, 0)
+
+        # plt.show()
+
+        # Record findings
+        self.left_line.detected = True
+        self.left_line.current_fit = left_fit
+        self.right_line.detected = True
+        self.right_line.current_fit = right_fit
 
     def targeted_search(self, binary):
-        raise NotImplementedError
+        # Skip the sliding windows step once you know where the lines are
+        # Now you know where the lines are you have a fit! In the next frame
+        # of video you don't need to do a blind search again, but instead you
+        # can just search in a margin around the previous line position
+        # like this:
+        left_fit = self.left_line.current_fit
+        right_fit = self.right_line.current_fit
+        # Assume you now have a new warped binary image
+        # from the next frame of video (also called "binary_warped")
+        # It's now much easier to find line pixels!
+        nonzero = binary.nonzero()
+        nonzeroy = np.array(nonzero[0])
+        nonzerox = np.array(nonzero[1])
+        margin = 100
+        left_lane_inds = ((nonzerox > (left_fit[0] * (nonzeroy**2) + left_fit[1] * nonzeroy + left_fit[2] - margin)) & (nonzerox < (left_fit[0] * (nonzeroy**2) + left_fit[1] * nonzeroy + left_fit[2] + margin)))
+        right_lane_inds = ((nonzerox > (right_fit[0] * (nonzeroy**2) + right_fit[1] * nonzeroy + right_fit[2] - margin)) & (nonzerox < (right_fit[0] * (nonzeroy**2) + right_fit[1] * nonzeroy + right_fit[2] + margin)))
+
+        # Again, extract left and right line pixel positions
+        leftx = nonzerox[left_lane_inds]
+        lefty = nonzeroy[left_lane_inds]
+        rightx = nonzerox[right_lane_inds]
+        righty = nonzeroy[right_lane_inds]
+        # Fit a second order polynomial to each
+        left_fit = np.polyfit(lefty, leftx, 2)
+        right_fit = np.polyfit(righty, rightx, 2)
+        # Generate x and y values for plotting
+        ploty = np.linspace(0, binary.shape[0]-1, binary.shape[0])
+        left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+        right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+
+        # And you're done! But let's visualize the result here as well
+        # Create an image to draw on and an image to show the selection window
+        out_img = np.dstack((binary, binary, binary)) * 255
+        window_img = np.zeros_like(out_img)
+        # Color in left and right line pixels
+        out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
+        out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
+
+        # Generate a polygon to illustrate the search window area
+        # And recast the x and y points into usable format for cv2.fillPoly()
+        left_line_window1 = np.array([np.transpose(np.vstack([left_fitx-margin, ploty]))])
+        left_line_window2 = np.array([np.flipud(np.transpose(np.vstack([left_fitx + margin, ploty])))])
+        left_line_pts = np.hstack((left_line_window1, left_line_window2))
+        right_line_window1 = np.array([np.transpose(np.vstack([right_fitx-margin, ploty]))])
+        right_line_window2 = np.array([np.flipud(np.transpose(np.vstack([right_fitx + margin, ploty])))])
+        right_line_pts = np.hstack((right_line_window1, right_line_window2))
+
+        # Draw the lane onto the warped blank image
+        cv2.fillPoly(window_img, np.int_([left_line_pts]), (0, 255, 0))
+        cv2.fillPoly(window_img, np.int_([right_line_pts]), (0, 255, 0))
+        result = cv2.addWeighted(out_img, 1, window_img, 0.3, 0)
+        plt.imshow(result)
+        plt.plot(left_fitx, ploty, color='yellow')
+        plt.plot(right_fitx, ploty, color='yellow')
+        plt.xlim(0, 1280)
+        plt.ylim(720, 0)
+
+        # plt.show()
 
     def search(self, binary):
         if (self.left_line.detected | self.right_line.detected):
@@ -168,8 +323,8 @@ def gradient_threshold(img):
     hls_bin = hls_s_thresh(img, thresh=(170, 255))
     # Combine the two binary thresholds
     binary = np.zeros_like(abs_bin, dtype=np.uint8)
-    binary[(abs_bin == 1) | (hls_bin == 1)] = 1
-    return binary, abs_bin, hls_bin, mag_bin, dir_bin
+    binary[((mag_bin == 1) & (dir_bin == 1)) | (hls_bin == 1)] = 1
+    return binary, (abs_bin, hls_bin, mag_bin, dir_bin)
 
 
 def setup(config_file='config.json'):
@@ -233,6 +388,7 @@ def test_threshold(img, abs_bin, hls_bin, mag_bin, dir_bin):
     ax2.imshow(stacks)
     plt.show()
 
+
 def test_perspective(img, camera):
     with open('config.json') as f:
         config = json.load(f)
@@ -245,6 +401,20 @@ def test_perspective(img, camera):
     plt.subplot(212)
     plt.imshow(plan)
     plt.show()
+
+
+def test_lane_search(file):
+    binary = cv2.imread(file)
+    binary = binary[:, :, 0]
+    binary = binary / 255
+    binary = binary.astype(np.uint8)
+    lane = Lane(width=1.0)
+    plt.subplot(211)
+    lane.search(binary)
+    plt.subplot(212)
+    lane.search(binary)
+    plt.show()
+
 
 if __name__ == '__main__':
     main()
